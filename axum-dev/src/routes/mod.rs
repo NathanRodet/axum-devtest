10df786423ca0d0dda2c738b87ad345ee64bd9af
Nathan Rodet <rodetnathan.contact@gmail.com>
2022-11-29 14:35:37 +0100

    added few updates

--- axum-dev/src/routes/mod.rs
+++ axum-dev/src/routes/mod.rs
@@ -1,19 +1,25 @@
-use axum::{body::Body, routing::{get, post}, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 mod hello_world;
-mod post_string;
-mod post_json;
 mod path_variables;
+mod post_json;
+mod post_string;
+mod query_params;
 
 use hello_world::hello_world;
-use post_string::post_string;
-use post_json::post_json;
 use path_variables::path_variables;
+use post_json::post_json;
+use post_string::post_string;
+use query_params::query_params;
 
-pub fn create_routes() -> Router<Body> {
+pub fn create_routes() -> Router {
     Router::new()
         .route("/", get(hello_world))
         .route("/post_string", post(post_string))
         .route("/post_json", post(post_json))
         .route("/path_variables/:id", get(path_variables))
+        .route("/query_params", get(query_params))
 }
